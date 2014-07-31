baec8b3eace14e54652c69f9716edf25dc5c1ddc
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2014-07-31 19:39:53 -0300

    admin for polls

--- dj17_test/polls/admin.py
+++ dj17_test/polls/admin.py
@@ -1,3 +1,7 @@
+from __future__ import absolute_import
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
+
+admin.site.register(Question, admin.ModelAdmin)
+admin.site.register(Choice, admin.ModelAdmin)
